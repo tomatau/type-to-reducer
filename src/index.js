00ad5fd01a1c6bd9185531f04a777b13c554a80a
Kishore Varma <kishore.nadimpalli@orionhealth.com>
2016-11-02 15:04:27 +1300

    removed lodash methods

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import { reduce, isFunction } from 'lodash'
-
 export default function typeToReducer(reducerMap, initialState) {
   const makeType = (prefix, type) => prefix.concat(type).join('_')
 
-  const iterator = (reducers, initial={}, prefix=[]) =>
-    reduce(reducers, (acc, reducer, type) =>
-      isFunction(reducer)
-        ? { ...acc, [makeType(prefix, type)]: reducer }
-        : iterator(reducer, acc, [ makeType(prefix, type) ])
-    , initial)
+  const iterator = (reducers, initial={}, prefix=[]) => {
+    const reducerTypes = Object.keys(reducers)
+    return reducerTypes.reduce((acc, type) => {
+      const reducer = reducers[type]
+      return typeof(reducer) === 'function'
+        ? {...acc, [makeType(prefix, type)]: reducer}
+        : iterator(reducer, acc, [makeType(prefix, type)])
+    }, initial)
+  }
 
   const flattened = iterator(reducerMap)
 
