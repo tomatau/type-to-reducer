3ae8db19a204ac93c2a87c0802051936d35fd2ad
hsz <jakub@chrzanowski.info>
2019-02-20 14:08:47 +0100

    TypeScript - allow to specify state and action types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,14 +1,18 @@
 interface reducerMapFunction<S, A> {
-  (state: S, action: A): S
+  (state: S, action?: A): S;
+}
+
+interface reducerMapReturnFunction<S, A> {
+  (state: S | undefined, action?: A): S;
 }
 
 interface reducerMap<S, A> {
-  [key: string]: reducerMap<S, A> | reducerMapFunction<S, A>
+  [key: string]: reducerMap<S, A> | reducerMapFunction<S, A>;
 }
 
 declare module 'type-to-reducer' {
-  export default function typeToReducer<S = any, A = any> (
+  export default function typeToReducer<S, A = any>(
     reducerMap: reducerMap<S, A>,
-    initialState: S
-  ): reducerMapFunction<S, A>
+    initialState: S,
+  ): reducerMapReturnFunction<S, A>;
 }
