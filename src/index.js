1acf4858629fa8a54766126b565f9bddbbeeb2ab
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2019-08-24 15:38:14 +0100

    updates code a little and adds some comments

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
+// internal state
 let typeDelimiter = '_'
 
+// change the string separating action types when reducerMap is nested
 export const setTypeDelimiter = customTypeDelimiter => typeDelimiter = customTypeDelimiter
 
+// function to concatenate any nested types
+const _makeType = (prefix, type) => prefix.concat(type).join(typeDelimiter)
+
+// iterator function that will read the reducerMap and return a flattened object
+const _flattenReducerMap = (reducers, initial={}, prefix=[]) => {
+  const reducerTypes = Object.keys(reducers || {})
+  return reducerTypes.reduce((acc, type) => {
+    const reducer = reducers[type]
+    return typeof(reducer) === 'function'
+      ? { ...acc, [_makeType(prefix, type)]: reducer }
+      : _flattenReducerMap(reducer, acc, [ _makeType(prefix, type) ])
+  }, initial)
+}
+
 export default function typeToReducer(reducerMap, initialState) {
-  const makeType = (prefix, type) => prefix.concat(type).join(typeDelimiter)
-
-  const iterator = (reducers, initial={}, prefix=[]) => {
-    const reducerTypes = Object.keys(reducers || {})
-    return reducerTypes.reduce((acc, type) => {
-      const reducer = reducers[type]
-      return typeof(reducer) === 'function'
-        ? { ...acc, [makeType(prefix, type)]: reducer }
-        : iterator(reducer, acc, [ makeType(prefix, type) ])
-    }, initial)
-  }
-
-  const flattened = iterator(reducerMap)
+  const flattened = _flattenReducerMap(reducerMap)
 
   return (state=initialState, action) => {
     const reducer = flattened[action.type]
